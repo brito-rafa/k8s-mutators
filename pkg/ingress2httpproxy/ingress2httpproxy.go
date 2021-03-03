01c29abdcac5022c822f20ed491ef1afc1c57e3b
drajshek <rajdevdurg@gmail.com>
2021-03-04 01:07:31 +0530

    Ingress2HTTPProxy  (#4)
    implement ingress2http mutator

---
+++ pkg/ingress2httpproxy/ingress2httpproxy.go
@@ -0,0 +1,123 @@
+package ingress2httpproxy
+
+import (
+	"strings"
+
+	contour "github.com/projectcontour/contour/apis/projectcontour/v1"
+	"github.com/sirupsen/logrus"
+	networking "k8s.io/api/networking/v1beta1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	unsupportedHosts = "unsupported-hosts"
+)
+
+// MutatorOutput contains the mutated output structures
+type MutatorOutput struct {
+	HTTPProxy contour.HTTPProxy
+}
+
+// Mutator contains common atttributes and the mutation input source structure
+type Mutator struct {
+	name   string
+	log    logrus.FieldLogger
+	input  networking.Ingress
+	domain string
+}
+
+// NewMutator creates a new Mutator. Clients of this API should set a meaningful name that can be used
+// to easily identify the calling client.
+func NewMutator(name string, log logrus.FieldLogger, ingress networking.Ingress, domain string) Mutator {
+	return Mutator{
+		name:   name,
+		log:    log,
+		input:  ingress,
+		domain: domain,
+	}
+}
+
+// Mutate converts a Ingress into HTTPProxy
+func (m *Mutator) Mutate() *MutatorOutput {
+	return &MutatorOutput{
+		HTTPProxy: m.buildHTTPProxy(),
+	}
+}
+
+// buildHTTPProxy takes ingress object as an input and returns  Contour HTTPProxy
+func (m *Mutator) buildHTTPProxy() contour.HTTPProxy {
+	var httpAnnotations = make(map[string]string)
+	hpTranslatedRoute, httpAnnotations := m.createRoute(httpAnnotations)
+	hp := contour.HTTPProxy{
+		TypeMeta: meta.TypeMeta{
+			Kind:       "HTTPProxy",
+			APIVersion: "projectcontour.io/v1",
+		},
+		ObjectMeta: meta.ObjectMeta{
+			Name:        m.input.ObjectMeta.Name,
+			Annotations: httpAnnotations,
+			Namespace:   m.input.ObjectMeta.Namespace,
+		},
+		Spec: contour.HTTPProxySpec{
+			Routes: hpTranslatedRoute,
+		},
+	}
+
+	httpProxyFqdn := m.input.Spec.Rules[0].Host
+	if m.domain != "" {
+		normalizedDomain := m.domain
+		if m.domain[0:2] == "*." {
+			normalizedDomain = m.domain[2:]
+		}
+		if m.domain[0:1] == "." {
+			normalizedDomain = m.domain[1:]
+		}
+		prefix := strings.SplitN(m.input.Spec.Rules[0].Host, ".", 2)
+		httpProxyFqdn = prefix[0] + "." + normalizedDomain
+	} else {
+		m.log.Warnf(
+			"[%s] No new wildcard DNS domain specified, use original Ingress host domain %s.",
+			m.name,
+			m.input.Spec.Rules[0].Host,
+		)
+	}
+
+	hp.Spec.VirtualHost = &contour.VirtualHost{
+		Fqdn: httpProxyFqdn,
+	}
+
+	if m.input.Spec.TLS[0].SecretName != "" {
+		hp.Spec.VirtualHost.TLS = &contour.TLS{}
+		hp.Spec.VirtualHost.TLS.SecretName = m.input.Spec.TLS[0].SecretName
+	}
+	return hp
+}
+
+// createRoute creates the route object which includes conditions and service details
+func (m *Mutator) createRoute(httpAnnotations map[string]string) ([]contour.Route, map[string]string) {
+	inrules := m.input.Spec.Rules
+	routes := make([]contour.Route, len(inrules[0].HTTP.Paths))
+	for i, path := range inrules[0].HTTP.Paths {
+		routes[i].Conditions = []contour.MatchCondition{
+			{Prefix: path.Path},
+		}
+		routes[i].Services = []contour.Service{
+			{
+				Name: path.Backend.ServiceName,
+				Port: path.Backend.ServicePort.IntValue(),
+			},
+		}
+	}
+
+	// Check if multiple rules present in Ingress object
+	if len(inrules) > 1 {
+		hosts := make([]string, 0, len(inrules)-1)
+		for _, inrule := range inrules[1:] {
+			hosts = append(hosts, inrule.Host)
+		}
+		m.log.Infof("[%s] unsupported hosts: %s", m.name, strings.Join(hosts, ", "))
+		httpAnnotations[m.name+"/"+unsupportedHosts] = strings.Join(hosts, ", ")
+	}
+
+	return routes, httpAnnotations
+}
---
+++ pkg/ingress2httpproxy/ingress2httpproxy_test.go
@@ -0,0 +1,88 @@
+package ingress2httpproxy
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	networking "k8s.io/api/networking/v1beta1"
+)
+
+const clientName = "testClient"
+
+func TestBuildHTTPProxy(t *testing.T) {
+	type output struct {
+		httpProxy  string
+		apiVersion string
+	}
+
+	tests := []struct {
+		name   string
+		input  string
+		want   output
+		domain string
+	}{
+		{
+			"Test config map with *.domain",
+			"example_with_wildcard.json",
+			output{
+				httpProxy:  "HTTPProxy",
+				apiVersion: "projectcontour.io/v1",
+			},
+			"*.migrator.servicemesh.biz",
+		},
+		{
+			"Test config map with .domain",
+			"example_with_wildcard.json",
+			output{
+				httpProxy:  "HTTPProxy",
+				apiVersion: "projectcontour.io/v1",
+			},
+			".migrator.servicemesh.biz",
+		},
+		{
+			"Test config map with .domain",
+			"example_without_wildcard.json",
+			output{
+				httpProxy:  "HTTPProxy",
+				apiVersion: "projectcontour.io/v1",
+			},
+			"cafe.migrator.servicemesh.biz",
+		},
+	}
+
+	for _, tc := range tests {
+		m := newMutatorFromFileData(t, tc.input, tc.domain, tc.name)
+		hp := m.buildHTTPProxy()
+
+		assert.Equal(t, tc.want.httpProxy, hp.Kind)
+		assert.Equal(t, tc.want.apiVersion, hp.APIVersion)
+		assert.Equal(t, m.input.Name, hp.Name)
+		assert.Equal(t, m.input.Spec.Rules[0].HTTP.Paths[0].Path, hp.Spec.Routes[0].Conditions[0].Prefix)
+		assert.Equal(t, m.input.Spec.Rules[0].HTTP.Paths[0].Backend.ServiceName, hp.Spec.Routes[0].Services[0].Name)
+		assert.Equal(t, m.input.Spec.Rules[0].HTTP.Paths[0].Backend.ServicePort.IntValue(), hp.Spec.Routes[0].Services[0].Port)
+		assert.Equal(t, m.input.Spec.Rules[0].HTTP.Paths[1].Path, hp.Spec.Routes[1].Conditions[0].Prefix)
+		assert.Equal(t, m.input.Spec.Rules[0].HTTP.Paths[1].Backend.ServiceName, hp.Spec.Routes[1].Services[0].Name)
+		assert.Equal(t, m.input.Spec.Rules[0].HTTP.Paths[1].Backend.ServicePort.IntValue(), hp.Spec.Routes[1].Services[0].Port)
+		assert.Equal(t, m.input.Spec.Rules[0].Host, hp.Spec.VirtualHost.Fqdn)
+	}
+}
+
+func newMutatorFromFileData(t *testing.T, fileName, domain, testName string) Mutator {
+	ingressFilePath := filepath.Join("testdata", fileName)
+	ingressFile, err := ioutil.ReadFile(ingressFilePath)
+	if err != nil {
+		t.Fatalf("%s: %v", testName, err)
+	}
+
+	ingress := networking.Ingress{}
+	err = json.Unmarshal(ingressFile, &ingress)
+	if err != nil {
+		t.Errorf("%s: unmarshall Ingress JSON = %v", testName, err)
+	}
+
+	return NewMutator(clientName, logrus.New(), ingress, domain)
+}
